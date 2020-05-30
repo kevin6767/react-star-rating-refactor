29487b66328785b4697a4ee68a25e3329945ff6f
Kevin Eslick <eslick.kevin@yahoo.com>
2020-05-30 17:18:41 -0500

    Updates

---
+++ precache-manifest.4c3e543dfa7167c9d37ef7c28afa7471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad1dbb63aaae07a956476bc8e2a7108",
+    "url": "/react-star-rating-refactor/index.html"
+  },
+  {
+    "revision": "4588dcf0042edc70c83c",
+    "url": "/react-star-rating-refactor/static/css/main.bfa1c16f.chunk.css"
+  },
+  {
+    "revision": "6d283f026a7696bac3d8",
+    "url": "/react-star-rating-refactor/static/js/2.ce02ba1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-star-rating-refactor/static/js/2.ce02ba1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4588dcf0042edc70c83c",
+    "url": "/react-star-rating-refactor/static/js/main.58bac3c8.chunk.js"
+  },
+  {
+    "revision": "02c82e4548236c37abe9",
+    "url": "/react-star-rating-refactor/static/js/runtime-main.1cc7bff4.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-star-rating-refactor/precache-manifest.4c3e543dfa7167c9d37ef7c28afa7471.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-star-rating-refactor/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
