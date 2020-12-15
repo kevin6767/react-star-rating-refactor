02ce12061e304b3f1add2b923589c89833fdf79e
Kevin Eslick <eslick.kevin@yahoo.com>
2020-12-14 23:30:23 -0600

    fixing file structure

--- react-flex/src/App.js
+++ react-flex/src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Rater from "./Rater";
+import Rater from './Rater';
+import Selector from './Selector';
 
-function App() {
-  return (
-    <div className="App">
-     <Rater />
-    </div>
-  );
+function App(props) {
+	const [iconValue, setIconValue] = useState(5);
+	const [value] = useState(100);
+	return (
+		<div className='App'>
+			<Selector setIconValue={setIconValue} value={value} />
+			<Rater iconValue={iconValue} value={value} />
+		</div>
+	);
 }
 
 export default App;
--- react-flex/src/Rater.js
+++ react-flex/src/Rater.js
@@ -1,73 +1,37 @@
-import React, { useState } from 'react'
-import {FaStarHalf} from "react-icons/all";
-import './Rater.css'
+import React, {useState} from 'react';
+import {FaStarHalf} from 'react-icons/all';
 
+import './Rater.css';
 
-const Rater = () => {
-    const [rating, setRating] = useState(null)
-    const [hover, setHover] = useState(null)
-    const [value] = useState(100)
-    const [iconValue, setIconValue] = useState(5)
+function Rater({iconValue}) {
+	const [rating, setRating] = useState(null);
+	const [hover, setHover] = useState(null);
 
+	return (
+		<div>
+			{[...Array(iconValue), ...Array(iconValue)].map((icon, i) => {
+				const value = i + 1;
 
-    return (
-        <div>
-            <select onChange={(e) =>
-            {setIconValue(Number(e.target.value))}}>
-                { Array.from(new Array(value),(value, index) =>
-                    index + 1).map(value => <option
-                    key={value} value={value}>{value}</option>) }
-            </select>
-            <h1> Select Amount of Icons </h1>
-
-
-            {[... Array(iconValue)].map((icon, i) => {
-                const value = i + 1
-
-                return (
-                    <label>
-                        <input type='radio'
-                               name='rating'
-                               value={value}
-                               onClick={() => setRating(value)}
-
-
-                        />
-                        <FaStarHalf
-                        className='star'
-                        color={value <= (hover || rating) ? '#ffc107' : '#e4e5e9'}
-                        size={100}
-                        onMouseEnter={() => setHover(value)}
-                        onMouseLeave={() => setHover(null)}
-                    />
-                    </label>
-                )
-            })}
-            {[... Array(iconValue)].map((icon, i) => {
-                const value = i + 1
-
-                return (
-                    <label>
-                        <input type='radio'
-                               name='rating'
-                               value={value}
-                               onClick={() => setRating(value)}
-
-
-                        />
-                        <FaStarHalf
-                            className='star-left'
-                            color={value <= (hover || rating) ? '#ffc107' : '#e4e5e9'}
-                            size={100}
-                            onMouseEnter={() => setHover(value)}
-                            onMouseLeave={() => setHover(null)}
-                        />
-                    </label>
-                )
-            })}
-        </div>
-    )
-
+				return (
+					<label>
+						<input
+							type='radio'
+							name='rating'
+							value={value}
+							onClick={() => setRating(value)}
+						/>
+						<div className='star-container'>
+							<FaStarHalf
+								className={i % 2 ? 'star-left' : 'star'}
+								color={value <= (hover || rating) ? '#ffc107' : '#e4e5e9'}
+								onMouseEnter={() => setHover(value)}
+								onMouseLeave={() => setHover(null)}
+							/>
+						</div>
+					</label>
+				);
+			})}
+		</div>
+	);
 }
-
-export default Rater
+export default Rater;
